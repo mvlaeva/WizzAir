88b1c7e39b3e464ee478c1cd9f7df99983aea4af
Dev-1 <Dev-1>
2016-10-01 19:12:29 +0300

    Database added. Class Flight modified

--- src/main/java/com/wizzair/Flight.java
+++ src/main/java/com/wizzair/Flight.java
@@ -10,5 +10,56 @@ public class Flight {
 	LocalDate dateAndTime;
 	List<Passanger> passangers;
 	Map<String ,Boolean> seats;
+	
+	public Flight(String origin, String destination, LocalDate dateAndTime, List<Passanger> passangers,
+			Map<String, Boolean> seats) {
+		this.origin = origin;
+		this.destination = destination;
+		this.dateAndTime = dateAndTime;
+		this.passangers = passangers;
+		this.seats = seats;
+	}
 
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public LocalDate getDateAndTime() {
+		return dateAndTime;
+	}
+
+	public void setDateAndTime(LocalDate dateAndTime) {
+		this.dateAndTime = dateAndTime;
+	}
+
+	public List<Passanger> getPassangers() {
+		return passangers;
+	}
+
+	public void setPassangers(List<Passanger> passangers) {
+		this.passangers = passangers;
+	}
+
+	public Map<String, Boolean> getSeats() {
+		return seats;
+	}
+
+	public void setSeats(Map<String, Boolean> seats) {
+		this.seats = seats;
+	}
+
+	
+	
 }
